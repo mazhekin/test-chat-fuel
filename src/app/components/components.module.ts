6d55013e98002b30c752a083a92491934111c3d9
mazhekin <mazhekin@gmail.com>
2017-12-18 15:00:44 +0200

    header added

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,16 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {HeaderComponent} from "./layout/header/header.component";
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class ComponentsModule { }
